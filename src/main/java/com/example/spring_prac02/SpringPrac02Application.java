de828df560ced562c8160a81ef5f4314c509947e
kyungwoon <nakw0410@gmail.com>
2022-01-29 23:04:55 +0900

    initial commit

---
+++ src/main/java/com/example/spring_prac02/SpringPrac02Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_prac02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrac02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPrac02Application.class, args);
+    }
+
+}
