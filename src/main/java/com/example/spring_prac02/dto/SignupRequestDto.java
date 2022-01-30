82909b041b4fb5573dcda5882a7f50bd49a83b83
kyungwoon <nakw0410@gmail.com>
2022-01-30 22:08:30 +0900

    feat: 스프링 시큐리티 유효성 검사

--- src/main/java/com/example/spring_prac02/dto/SignupRequestDto.java
+++ src/main/java/com/example/spring_prac02/dto/SignupRequestDto.java
@@ -3,11 +3,30 @@ package com.example.spring_prac02.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Setter
 @Getter
 public class SignupRequestDto {
+
     private String username;
+    @NotBlank(message = "이름을 입력해주세요.")
+
+    @NotBlank(message = "닉네임을 입력해주세요.")
+    @Size(min = 3, message = "닉네임은 3글자 이상 입력해주세요.")
+    private String nickname;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Size(min = 4, message = "비밀번호를 4글자 이상 입력해주세요.")
     private String password;
+
+    @NotBlank(message = "비밀번호 확인을 입력해주세요.")
+    @Size(min = 4, message = "비밀번호를 4글자 이상 입력해주세요.")
+    private String checkPassword;
+
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "이메일 형식에 맞게 입력해주세요.")
     private String email;
-    private boolean admin = false;
 }
