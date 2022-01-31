dc9916646d090aba6573f0f57c23e2a4987d49cc
kyungwoon <nakw0410@gmail.com>
2022-02-01 02:53:32 +0900

    feat: HomeController

--- src/main/java/com/example/spring_prac02/controller/HomeController.java
+++ src/main/java/com/example/spring_prac02/controller/HomeController.java
@@ -1,25 +1,41 @@
 package com.example.spring_prac02.controller;
 
+import com.example.spring_prac02.model.Post;
 import com.example.spring_prac02.security.UserDetailsImpl;
+import com.example.spring_prac02.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
 
+
+@RequiredArgsConstructor
 @Controller
 public class HomeController {
 
+    private final PostService postService;
+
     @GetMapping("/")
     public String home(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-            try {
-                String username = userDetails.getUsername();
-                if(username != null) {
-                    model.addAttribute("nickname", username);
-                }
-            } catch(Exception e) {
-                System.out.println(e.getMessage());
+        System.out.println("public String home(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) 시작 >>>>");
+        try {
+            if(userDetails != null) {
+                System.out.println("userId: " + userDetails.getUser().getId());
+                model.addAttribute("name", userDetails.getUser().getName()); //사용자 이름
+                model.addAttribute("userId", userDetails.getUser().getId()); //USER테이블 고유번호
             }
-            return "index";
+
+            List<Post> postList = postService.getPosts();
+            model.addAttribute("postList", postList);
+
+        } catch(Exception e) {
+
         }
+
+        return "index";
     }
+
+}
